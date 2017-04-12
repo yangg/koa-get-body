b4d7e1d6044cab9145f46ba6c76936e9cf5912b0
Brook Yang <uedsky@gmail.com>
2017-04-12 15:52:03 +0800

    add multipart test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -25,18 +25,51 @@ describe('koa-get-body', function () {
       .expect({name: 'koa-get-body'}, done)
     })
 
+    it('should parse json body ok', function (done) {
+      app.use(getBody())
+
+      app.use(async ctx => {
+        const body = await ctx.request.getBody()
+        expect(body).toEqual({ name: 'koa-get-body' })
+        ctx.body = body
+      })
+      request(app.listen())
+      .post('/')
+      .set('Content-type', 'application/vnd.api+json')
+      .send({name: 'koa-get-body'})
+      .expect({name: 'koa-get-body'}, done)
+    })
+
     it('should parse form body ok', function (done) {
       app.use(getBody())
 
       app.use(async ctx => {
         const body = await ctx.request.getBody()
-        expect(body).toEqual({ name: 'koa-get-body', foo: 'bar' })
+        expect(body).toEqual({ name: 'koa-get-body' })
         ctx.body = body
       })
       request(app.listen())
       .post('/')
-      .send('name=koa-get-body&foo=bar')
-      .expect({name: 'koa-get-body', foo: 'bar'}, done)
+      .type('form')
+      .send({ name: 'koa-get-body' }) // foo: { bar: 'barz' }, tags: ['node', 'koa']
+      .expect({ name: 'koa-get-body' }, done)
+    })
+
+    it('should parse multipart ok', function (done) {
+      app.use(getBody())
+
+      app.use(async ctx => {
+        const body = await ctx.request.getBody()
+        expect(ctx.request.type).toEqual('multipart/form-data')
+        expect(body.name).toEqual('koa-get-body')
+        expect(body.avatar.fileSize).toEqual(43)
+        ctx.body = body
+      })
+      request(app.listen())
+      .post('/')
+      .field('name', 'koa-get-body')
+      .attach('avatar', 'spec/avatar.gif')
+      .expect('name', 'koa-get-body', done)
     })
   })
 })
