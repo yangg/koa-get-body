47ac44197506f63059289ff1eb34dab58214193c
Brook Yang <uedsky@gmail.com>
2017-04-12 08:55:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# koa-get-body
+A body parser for koa2, based on [busboy](https://github.com/mscdex/busboy), support `urlencoded`, `multipart` and `json`
+
+[![Build Status](https://travis-ci.org/yangg/koa-get-body.svg?branch=master)](https://travis-ci.org/yangg/koa-get-body)
+[![npm:](https://img.shields.io/npm/v/koa-get-body.svg?style=flat)](https://www.npmjs.com/packages/koa-get-body)
+
+## Installation
+```bash
+yarn add koa-get-body
+```
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,95 @@
+
+const os = require('os')
+const Busboy = require('busboy')
+const fs = require('fs')
+const path = require('path')
+
+function main (options) {
+  Object.assign({}, options)
+  return async function (ctx, next) {
+    let formData
+    ctx.request.getBody = getBody
+    options.alias && (ctx[options.alias] = getBody)
+    return await next()
+    async function getBody (name, defaultValue = '') {
+      if (!formData) {
+        if (ctx.is('multipart/*', 'urlencoded')) {
+          formData = await formParser(ctx.req, Object.assign({ headers: ctx.req.headers }, options))
+        } else if (ctx.is('json')) {
+          formData = await jsonParser(ctx.req)
+        } else {
+          formData = Promise.resolve({})
+        }
+      }
+      if (typeof name !== 'undefined') {
+        return formData.hasOwnProperty(name) ? formData[name] : defaultValue
+      }
+      return formData
+    }
+  }
+}
+
+function jsonParser (req) {
+  return new Promise(resolve => {
+    let fullBody = ''
+    req.on('data', chunk => {
+      fullBody += chunk.toString()
+    })
+    req.on('end', () => {
+      resolve(JSON.parse(fullBody))
+    })
+  })
+}
+
+function formParser (req, options) {
+  const busboy = new Busboy(options)
+  return new Promise((resolve, reject) => {
+    let formData = {}
+    let hasError
+    busboy.on('file', function (fieldName, stream, filename, encoding, mimeType) {
+      // save tmp files
+      const tmpDir = options.uploadDir ? options.uploadDir : os.tmpdir()
+      const tmpPath = path.join(tmpDir + path.basename(fieldName))
+      let fileSize = 0
+      stream.pipe(fs.createWriteStream(tmpPath))
+      stream.on('data', function (data) {
+        fileSize += data.length
+      })
+      stream.on('end', function () {
+        // push file data
+        formData[fieldName] = fileSize > 0 ? {
+          fileName: filename,
+          fileSize: fileSize,
+          mimeType: mimeType,
+          tmpPath: tmpPath
+        } : null
+      })
+      stream.on('limit', function () {
+        hasError = 'fileSizeLimit'
+      })
+    })
+    busboy.on('field', function (fieldName, val) {
+      // push text data
+      formData[fieldName] = val
+    })
+    busboy.on('partsLimit filesLimit fieldsLimit', function () {
+      hasError = 'partsLimit'
+    })
+    busboy.on('filesLimit', function () {
+      hasError = 'filesNumberLimit'
+    })
+    busboy.on('fieldsLimit', function () {
+      hasError = 'fieldsLimit'
+    })
+    busboy.on('finish', function () {
+      if (hasError) {
+        reject(new Error(hasError))
+      } else {
+        resolve(formData)
+      }
+    })
+    req.pipe(busboy)
+  })
+}
+
+module.exports = main
